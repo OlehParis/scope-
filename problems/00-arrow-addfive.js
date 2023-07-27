c6c397ec3554661f6625240d2bbc41ac8f29454a
Oleh <olehparis@Olehs-Air.attlocal.net>
2023-07-27 16:45:26 -0400

    sddd

--- problems/00-arrow-addfive.js
+++ problems/00-arrow-addfive.js
@@ -17,7 +17,7 @@ let result3 = addFive(37); // returns 42
 
 // Your code here
 
-
+addFive (num) => {num + 5};
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = addFive;
